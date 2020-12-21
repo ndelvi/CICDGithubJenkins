9696e560edec9086741d19f83ae63e4a422650fa
ndelvi <73512025+ndelvi@users.noreply.github.com>
2020-12-20 20:18:30 -0500

    Add files via upload
    Added a new TestGhubJenkinCICD.java from my local machine to repository this is my first commit.

---
+++ TestGhubJenkinCICD.java
@@ -0,0 +1,8 @@
+public class TestGhubJenkinCICD {
+
+	  public static void main(String[] args) {
+	    
+	    System.out.println(" ---->>>> Hi Everyone this is Github <<<<---- ");
+	    System.out.println(" ---->>>>  and Jenkins CI / CD Test  <<<<---- ");	
+	  }	
+}
